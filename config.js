db55d9c521a7a17f179c11ae468bc713fb793a2d
JadlionHD <theendermanblue@gmail.com>
2020-10-21 14:00:20 +0700

    k

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
      "TOKEN" : process.env.DISCORD_TOKEN,
-     "PREFIX" : "h;",
+     "PREFIX" : "j!",
      "ownerID": ["421307985827201024", "540560193277132800"],
      "colors": {
         "error": 0xFF0000,
