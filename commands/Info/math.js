a6552f382eaa77bfffcde118290c8209f2d1bec9
JadlionHD <theendermanblue@gmail.com>
2020-12-15 16:14:33 +0700

    math

--- commands/Info/math.js
+++ commands/Info/math.js
@@ -1,11 +1,15 @@
 const mathjs = require("mathjs");
 
 exports.run = function(client, msg, args) {
+	let arg = args.join(" ");
 
-	if(!args.join(" ") || args.join(" ").includes(":") || args.join(" ").includes("config"))
+	if(!arg || arg.includes(":") || arg.includes("config"))
 		return msg.channel.createMessage("<:cross:762537848691752960> Please enter a valid number that you want to calculate");
-	if(args.join(" ").length > 150)
+	if(arg.length > 150)
 		return msg.channel.createMessage("Woah woah woah, That's too much numbers");
+	let result = mathjs.evaluate(arg).toString();
+	if(result.includes("function"))
+		result = `[Function: ${arg}] use ${arg}(arguments) instead.`;
 	try {
 		let msgEmbed = {
 			embed: {
@@ -13,11 +17,11 @@ exports.run = function(client, msg, args) {
 				description: `
 **Math Question:**
 \`\`\`yaml
-${args.join(" ")}
+${arg}
 \`\`\`
 **Math Answer:**
 \`\`\`yaml
-${mathjs.evaluate(args.join(" "))}
+${result}
 \`\`\`
 `
 			}
