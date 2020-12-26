7c7fc54930b51ea511975de3379cf1d59cbf15f5
JadlionHD <theendermanblue@gmail.com>
2020-12-26 15:54:33 +0700

    using restana instead

--- misc/api/routes.js
+++ misc/api/routes.js
@@ -1,10 +1,10 @@
-const app = require("express")();
+const app = require("restana")();
 
 app.get("/", (req, res) => {
 	res.send("https://github.com/JadlionHD");
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.start(process.env.PORT || 3000, () => {
 	console.log("Running express web");
 });
 
